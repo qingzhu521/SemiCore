da6adff23f87b829fee8804ed455339fa0158527
forrabbit <iceforrabbit@gmail.com>
2015-05-20 15:38:14 +1000

    edit parameter

--- betaF/main.cpp
+++ betaF/main.cpp
@@ -9,7 +9,7 @@ int main(){
 	
 	Application* app = new Application("/Users/dongwen/workspace/Hummingbird/");
 
-	// app->sortEdge("/Users/dongwen/workspace/Hummingbird/dataset/uk-2002.txt");
+	// app->sortEdge("/Users/dongwen/workspace/Hummingbird/dataset/soc-pokec-relationships.txt");
 	
 	app->semiKCore();
 	app->printCoreDistribution();
