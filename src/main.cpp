c676b0bfaa848c30e67a1e5441a310ffc4f95003
DongWen <iceforrabbit@gmail.com>
2015-11-18 21:39:41 +1100

    final

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,23 @@
 using namespace std;
 
 int main(){
-	
+	// This is the path of graph file (graph.idx, graph.dat and graph.info files)
 	Application* app = new Application("/Users/DongWen/workspace/dataset/lj/");
 
-	//app->sortEdge("/Users/DongWen/workspace/Hummingbird/dataset/test.txt",100,true);
+	// process .txt file to graph.idx, graph.dat and graph.info
+	// first parameter is path of .txt
+	// second parameter is used to sample graph (100 means keep 100% of origin graph, i.e. don't sample)
+	// third parameter is sampling edges if true, or sampling nodes if false (it does not matter if second parameter is 100)
+	app->sortEdge("/Users/DongWen/workspace/kcore/dataset/test.txt",100,true);
 	
-	//app->getGraphInfo();
-	// app->semiKCore();
-	//app->semiKCoreNaivePlus();
-	//app->saveCore();
-	app->loadCore();
+	app->semiKCore();
+	// app->semiKCoreNaive();
+	// app->semiKCoreNaivePlus();
+	// app->saveCore();
 
-	app->dynamicCore(100);
+
+	// for dynamic graph
+	// app->dynamicCore(100);
 	
 	
 	app->printCoreDistribution();
